288a2a9f194d84454c23ed853664a05e5a2acd43
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-03 22:33:38 +0530

    updated

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,54 +1,54 @@
 import React from "react";
 import App from "../App";
 
-function StudentList(props) {
-  const students = [
-    {
-      id: 0,
-      Name: "Amal Perera",
-      age: 21,
-      course: "Information Technology",
-      isActive: Boolean,
-    },
-    {
-      id: 1,
-      Name: "Dilani Ranasinghe ",
-      age: 21,
-      course: "Information Technology",
-      isActive: Boolean,
-    },
-    {
-      id: 2,
-      Name: "Kasum Fernardo",
-      age: 21,
-      course: "Engineering Technology",
-      isActive: Boolean,
-    },
-    {
-      id: 3,
-      Name: "Nimesha Silva",
-      age: 21,
-      course: "Biology Engineering Technology",
-      isActive: Boolean,
-    },
-    {
-      id: 4,
-      Name: "Sanduni Jayawarthana",
-      age: 21,
-      course: "Information Technology",
-      isActive: Boolean,
-    },
-  ];
-  const list = students.map((StudentList) => (
-    <StudentList
-      key={StudentList.id}
-      name={StudentList.Name}
-      age={StudentList.age}
-      course={StudentList.course}
-      isActive={StudentList.isActive}
-    />
-  ));
-  return <>{list}</>;
+function StudentList() {
+  //   const students = [
+  //     {
+  //       id: 0,
+  //       Name: "Amal Perera",
+  //       age: 21,
+  //       course: "Information Technology",
+  //       isActive: Boolean,
+  //     },
+  //     {
+  //       id: 1,
+  //       Name: "Dilani Ranasinghe ",
+  //       age: 21,
+  //       course: "Information Technology",
+  //       isActive: Boolean,
+  //     },
+  //     {
+  //       id: 2,
+  //       Name: "Kasum Fernardo",
+  //       age: 21,
+  //       course: "Engineering Technology",
+  //       isActive: Boolean,
+  //     },
+  //     {
+  //       id: 3,
+  //       Name: "Nimesha Silva",
+  //       age: 21,
+  //       course: "Biology Engineering Technology",
+  //       isActive: Boolean,
+  //     },
+  //     {
+  //       id: 4,
+  //       Name: "Sanduni Jayawarthana",
+  //       age: 21,
+  //       course: "Information Technology",
+  //       isActive: Boolean,
+  //     },
+  //   ];
+  //   const list = students.map((StudentList) => (
+  //     <StudentList
+  //       key={StudentList.id}
+  //       name={StudentList.Name}
+  //       age={StudentList.age}
+  //       course={StudentList.course}
+  //       isActive={StudentList.isActive}
+  //     />
+  //   ));
+  return <></>;
 }
 
 export default StudentList;
